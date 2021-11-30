1a703ed776db744f1d0e6cb47b2c3c51c126e126
nama-aman <gun_aman@outlook.com>
2021-11-30 06:17:28 +0545

    added box with control

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 app.use(express.static(__dirname + '/public'))
 app.use('/box', express.static(__dirname + '/box'));
+app.use('/box_withcontrol', express.static(__dirname + '/box_withcontrol'));
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
